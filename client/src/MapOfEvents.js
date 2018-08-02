c3360899a0d78ae1b580b5216f675c92babca535
xCatnips <andrew.vmanzano@gmail.com>
2018-08-02 15:46:39 -0700

    Added in a functional google calendar for reference onto our map page and added some filler text to where our description will appear

--- client/src/MapOfEvents.js
+++ client/src/MapOfEvents.js
@@ -11,17 +11,28 @@ class MapOfEvents extends Component {
     return (
       <div>
         <NavBar />
-        <iframe src="https://www.google.com/maps/d/u/2/embed?mid=1ZdjvcSz5tbyURWV4dx5YgBA1PeN0wmtl" width="800" height="680"></iframe>
         <div>
-          <table className='Description'>
+          <table id='MapCalDes'>
             <tr>
-              <th> Sweet Alchemy </th>
-            </tr>
-            <tr>
-              <th> Price Range: $5-$20 </th>
-            </tr>
-            <tr>
-              <td colSpan="1"> A great and accesible location for all natural ice cream and candy. Well trained and supportive staff </td>
+              <td>
+                <iframe src="https://www.google.com/maps/d/u/2/embed?mid=1ZdjvcSz5tbyURWV4dx5YgBA1PeN0wmtl" width="800" height="680"></iframe>
+              </td>
+              <td>
+                <table id='CalDes'>
+                  <tr>
+                    <td>
+                      <iframe src="https://calendar.google.com/calendar/embed?src=lo1g5erva50mj5olpe940f1rgs%40group.calendar.google.com&ctz=America%2FLos_Angeles" width="400" height="400"></iframe>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <h1> Sweet Alchemy </h1>
+                      <h2> Price Range: $5-$20 </h2>
+                      <p> A great and accesible location for all-natural ice cream and candy. Well trained and supportive staff </p>
+                    </td>
+                  </tr>
+                </table>
+              </td>
             </tr>
           </table>
         </div>
