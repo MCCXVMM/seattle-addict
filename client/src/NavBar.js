4c439027bbd7aae18321e020d806fac26e33ac9e
W0ND3RF1LL3D <keni.michelle@gmail.com>
2018-08-20 13:41:41 -0700

    My shit boiii -Kendra

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -22,11 +22,6 @@ class NavBar extends Component {
                 <Skyline />
               </td>
               <td>
-<<<<<<< HEAD
-                  <iframe width="100%" height="200" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/300536103&color=%23421928&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-
-                <Links />
-=======
                 <table>
                   <tr>
                     {/* <td>
@@ -40,7 +35,6 @@ class NavBar extends Component {
                     </td>
                   </tr>
                 </table>
->>>>>>> 470167a1061e023d3d2ac334f4e651038fac4adc
               </td>
             </tr>
           </tbody>
